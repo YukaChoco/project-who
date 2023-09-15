66f44ef5c2e9b224237fa630df0e33b39cf79af9
YukaChoco <sugar.ore.yk@gmail.com>
2023-09-16 01:41:02 +0900

    必要ない画像を削除

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -40,23 +40,7 @@ export default function Home() {
         </div>
 
         <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
-          <div className={styles.thirteen}>
-            <Image
-              src="/thirteen.svg"
-              alt="13"
-              width={40}
-              height={31}
-              priority
-            />
-          </div>
+          <h1>StartPage</h1>
         </div>
 
         <div className={styles.grid}>
