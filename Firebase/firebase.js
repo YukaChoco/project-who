33df615a68c68df6fc927cb88fbee27d1204662b
YukaChoco <sugar.ore.yk@gmail.com>
2023-09-16 01:21:52 +0900

    Firebase/firebase.jsを追加

---
+++ Firebase/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCXmYcZaKGhoVVbPWRmR0QvebNgdLHbJz0",
+  authDomain: "who-797e5.firebaseapp.com",
+  projectId: "who-797e5",
+  storageBucket: "who-797e5.appspot.com",
+  messagingSenderId: "792930996511",
+  appId: "1:792930996511:web:1213c25af7f44a0a32a02a",
+  measurementId: "G-TJEEGY3EE4"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
