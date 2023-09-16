dfe5692f2d32cfe2c5b68c4d3c46b6316e9c870c
上野優衣 <uenoyui@uenoyuinoMacBook-Air.local>
2023-09-16 16:48:07 +0900

    made share button

--- src/conponents/ShareButton.tsx
+++ src/conponents/ShareButton.tsx
@@ -1,9 +1,22 @@
 import * as React from 'react';
 import ShareOutlinedIcon from '@mui/icons-material/ShareOutlined';
-import ShareIcon from "hackathon-2023-TeamA/public/share_button.svg";
+import styles from '@/styles/Login.module.css'
+import IconButton from '@mui/material/IconButton';
 
-export default function ShareButton(){
+export default function ShareButton() {
     return (
-      <ShareOutlinedIcon />
+        <IconButton sx={{
+            position: 'fixed',
+            bottom: 40, /* 基準の位置を画面の一番下に指定する */
+            right: 40, /* 基準の位置を画面の一番右に指定する */
+            background: "#6F80BF",
+            color: "white",
+            ":hover": { background: "#6F80BF" },
+        }}>
+            <ShareOutlinedIcon fontSize="large" />
+        </IconButton>
+
+
+
     );
-  }
+}
