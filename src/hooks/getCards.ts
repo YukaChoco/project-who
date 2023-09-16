fd5905b188b1a5bac99aba04dae75a7abdc23e9e
YukaChoco <sugar.ore.yk@gmail.com>
2023-09-17 03:38:00 +0900

    firebaseの操作色々試した

--- src/hooks/getCards.ts
+++
@@ -1,8 +0,0 @@
-import { doc, getDoc, getFirestore } from "firebase/firestore";
-import { db } from '../firebase'
-
-export default async function getCards() {
-  const docRef = doc(db, "user","hOvlmmbmHDuD92fk8ZZB");
-  const docSnap = await getDoc(docRef);
-  console.log(docSnap);
-}
