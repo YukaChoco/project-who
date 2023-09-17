316d8bcdbb501419427291de00af3192fc5bdf0e
shimochan <keita.s0412@icloud.com>
2023-09-18 06:00:17 +0900

    1枚目

--- src/conponents/Header.tsx
+++ src/conponents/Header.tsx
@@ -7,66 +7,75 @@ import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
 import SearchIcon from '@mui/icons-material/Search';
-import styles from '@/styles/Header.module.css'
+import styles from '@/styles/Header.module.css';
+import Head from 'next/head';
 
 interface Props {
-    useSearchIcon?: boolean;
-    useMenuIcon?: boolean;
-    onClick_edit?: () => void;
-    onClick_register?: () => void;
+  useSearchIcon?: boolean;
+  useMenuIcon?: boolean;
+  onClick_edit?: () => void;
+  onClick_register?: () => void;
 }
 
 export default function Header(props: Props) {
   return (
-    <div>
-    <Box sx={{ flexGrow: 1 }} className={styles.Header_container}>
-      <AppBar position="static" className={styles.Header_bar}>
-        <Toolbar>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            <a href="http://localhost:3000/cards">Who!</a>
-          </Typography>
-          {/* 1.検索アイコン */}
-          {
-            (props.useSearchIcon) &&
-            <IconButton
-              size="large"
-              color="inherit"
-              className={styles.Header_icon}
-            >
-              <a href="http://localhost:3000/upgrade"><SearchIcon /></a>
-            </IconButton>
-          }
+    <>
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link href="https://fonts.googleapis.com/css2?family=Kiwi+Maru:wght@300&family=Lemon&display=swap" rel="stylesheet" />
+      </Head>
 
-          {/* 2.メニューアイコン */}
-          {
-            (props.useMenuIcon) &&
-            <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            className={styles.Header_icon}
-            onClick={() => console.log('menuBtn Clicked')}
-          >
-            <MenuIcon />
-          </IconButton>
-          }
+      <div>
+        <Box sx={{ flexGrow: 1 }} className={styles.Header_container}>
+          <AppBar position="static" className={styles.Header_bar}>
+            <Toolbar>
+              <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                <a href="http://localhost:3000/cards" className={styles.font}>Who!</a>
+              </Typography>
+              {/* 1.検索アイコン */}
+              {
+                (props.useSearchIcon) &&
+                <IconButton
+                  size="large"
+                  color="inherit"
+                  className={styles.Header_icon}
+                >
+                  <a href="http://localhost:3000/upgrade"><SearchIcon /></a>
+                </IconButton>
+              }
 
-          {/* 3.編集完了ボタン */}
-          {
-            (props.onClick_edit != null) &&
-          <Button color="inherit" onClick={props.onClick_edit}>編集完了</Button>
-          }
+              {/* 2.メニューアイコン */}
+              {
+                (props.useMenuIcon) &&
+                <IconButton
+                  size="large"
+                  edge="start"
+                  color="inherit"
+                  aria-label="menu"
+                  className={styles.Header_icon}
+                  onClick={() => console.log('menuBtn Clicked')}
+                >
+                  <MenuIcon />
+                </IconButton>
+              }
 
-          {/* 4.登録ボタン */}
-          {
-            (props.onClick_register != null) &&
-          <Button color="inherit" onClick={props.onClick_register}>登録</Button>
-        }
-        </Toolbar>
-      </AppBar>
-    </Box>
-    <div className={styles.Header_margin}></div>
-    </div>
+              {/* 3.編集完了ボタン */}
+              {
+                (props.onClick_edit != null) &&
+                <Button color="inherit" onClick={props.onClick_edit}>編集完了</Button>
+              }
+
+              {/* 4.登録ボタン */}
+              {
+                (props.onClick_register != null) &&
+                <Button color="inherit" onClick={props.onClick_register}>登録</Button>
+              }
+            </Toolbar>
+          </AppBar>
+        </Box>
+        <div className={styles.Header_margin}></div>
+      </div>
+    </>
   );
 }
---
+++ src/pages/make/createText.tsx
@@ -0,0 +1,108 @@
+import { useState } from 'react'
+import { Inter } from 'next/font/google'
+import styles from '@/styles/creatText.module.css'
+import TextInput from '@/conponents/TextInput'
+import PrimaryBtn from '@/conponents/PrimaryBtn'
+import SecondaryBtn from '@/conponents/SecondaryBtn'
+import ColorPicker from '@/conponents/ColorPicker'
+import Header from '@/conponents/Header'
+import DisplayCard from '@/conponents/Card'
+import SwithButton from '@/conponents/SwitchButton'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Input() {
+        
+
+  const [name, setName] = useState<string>('');
+  const [x, setX] = useState<string>('');
+  const [instagram, setInstagram] = useState<string>('');
+  const [others, setOthers] = useState<string>('');
+  const [organization, setOrganization] = useState<string>('');
+  const [textColor, setTextColor] = useState<string>('');
+  const [bgColor, setBgColor] = useState<string>('');
+
+  function hundleName(event: React.ChangeEvent<HTMLInputElement>) {
+    setName(event.target.value);
+  }
+
+  function hundleX(event: React.ChangeEvent<HTMLInputElement>) {
+    setX(event.target.value);
+  }
+  function hundleOrganization(event: React.ChangeEvent<HTMLInputElement>) {
+    setOrganization(event.target.value);
+  }
+
+  function hundleTextColor(event: React.ChangeEvent<HTMLInputElement>) {
+    setTextColor(event.target.value);
+  }
+
+  function hundleBgColor(event: React.ChangeEvent<HTMLInputElement>) {
+    setBgColor(event.target.value);
+  }
+
+  function hundleinstagram(event: React.ChangeEvent<HTMLInputElement > ){
+    setInstagram(event.target.value);
+  }
+
+  // function hundleother(event: React.ChangeEvent<HTMLInputElement>) {
+  //   setOthers(event.target.value);
+  // }
+
+  return (
+    <>
+
+        <Header
+          onClick_edit={() => console.log('編集完了')}
+        />
+      
+
+      <main className={styles.main}>
+        
+        <div className={styles.preview}>
+          プレビュー
+        </div>
+    
+
+        <div className={styles.card}>
+       <DisplayCard  id={""} name={name} organization={organization} x={x} instagram={instagram} others={""} urlEnabled={false} textColor={''} bgColor={''} onClickHandler={function (): void {
+          throw new Error('Function not implemented.')
+        } }/>
+        </div>
+
+         <div className={styles.swith}> 
+        <SwithButton leftName={'入力'} rightName={'デザイン'} value={''}/>
+        </div>
+
+        <TextInput
+          text="氏名"
+          value={name}
+          onChange={hundleName}
+        />
+         <TextInput
+          text="instagram"
+          value={instagram}
+          onChange={hundleinstagram}
+        />
+        <TextInput
+          text="X"
+          value={x}
+          onChange={hundleX}
+        />
+        {/* <TextInput
+          text="その他"
+          value={others}
+          onChange={hundleother}
+        /> */}
+        <TextInput
+          text="所属団体"
+          value={organization}
+          onChange={hundleOrganization}
+        />
+        <div className={styles.space}/>
+        <SecondaryBtn text="編集完了" onClick={() => console.log('Secondarybtn Clicked')} />
+        
+      </main>
+    </>
+  )
+}
