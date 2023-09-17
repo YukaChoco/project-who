65151575827ebc0ce5ecfe2711a2620a884cc01a
shimochan <keita.s0412@icloud.com>
2023-09-18 07:23:38 +0900

    ログインユーザーのIDからMy_cardsのIDを取得

---
+++ src/utils/firebase/getMyCards.ts
@@ -0,0 +1,35 @@
+import { doc, getDoc } from "firebase/firestore";
+import { auth, db } from '@/firebase'
+import { getAuth, signInWithPopup, GoogleAuthProvider ,onAuthStateChanged } from "firebase/auth";
+
+import addUserData from './addUserDataComp'
+import AddMyCardComp from "./addMyCaredComp";
+import AddtoCardsComp from "./addtoCardscomp";
+import firestore from "./firestore";
+import getMyCardsComp from "./getMycardIdComp";
+
+
+export default async function getMyCards() {  
+
+    //現在ログインしているユーザーを取得する
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        // User is signed in, see docs for a list of available properties
+        // https://firebase.google.com/docs/reference/js/auth.user
+        const uid = user.uid;
+        const docData = {
+          userid: uid,
+        };
+        getMyCardsComp(docData);
+        
+      } else {
+        // User is signed out
+        // ...
+      }
+    });
+
+    
+}
+
+
+
