2d00a2e0ce6cd258eaa11391b182098c63c05235
YukaChoco <sugar.ore.yk@gmail.com>
2023-09-17 12:14:20 +0900

    テスト画面更新

--- src/conponents/NameInput.tsx
+++ src/conponents/NameInput.tsx
@@ -1,25 +1,30 @@
 import * as React from 'react';
 import TextField from '@mui/material/TextField';
 import styles from '@/styles/NameInput.module.css'
+import { TextFieldProps } from '@mui/material/TextField';
 
 interface Props {
-    text: string;
+  text: string;
+  value: string;
+  onChange: TextFieldProps['onChange'];
 }
 
 export default function NameInput(props: Props) {
   return (
     <>
-    <div className={styles.NameInput_background}>
-        <TextField  className={styles.NameInput} label={props.text} variant="filled"
-            sx={{
+      <div className={styles.NameInput_background}>
+        <TextField className={styles.NameInput} label={props.text} variant="filled"
+          sx={{
             input: {
-                color: "black",
-                background: "#F4F5FC"
+              color: "black",
+              background: "#F4F5FC"
             }
-            }}
-            focused
-            />
-    </div>
+          }}
+          value={props.value}
+          onChange={props.onChange}
+          // focused
+        />
+      </div>
     </>
   )
 }
--- src/pages/Test_Kotaro.tsx
+++ src/pages/Test_Kotaro.tsx
@@ -1,24 +1,57 @@
-import Head from 'next/head'
-import Image from 'next/image'
+import { useState } from 'react'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/TestKotaro.module.css'
 import NameInput from '@/conponents/NameInput'
 import PrimaryBtn from '@/conponents/PrimaryBtn'
 import SecondaryBtn from '@/conponents/SecondaryBtn'
-import SNSInput from '@/conponents/SNSInput'
 import ColorPicker from '@/conponents/ColorPicker'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Input() {
+
+  const [name, setName] = useState<string>('');
+  const [x, setX] = useState<string>('');
+  const [organization, setOrganization] = useState<string>('');
+  const [textColor, setTextColor] = useState<string>('');
+  const [bgColor, setBgColor] = useState<string>('');
+
+  function hundleName(event: React.ChangeEvent<HTMLInputElement>) {
+    setName(event.target.value);
+  }
+
+  function hundleX(event: React.ChangeEvent<HTMLInputElement>) {
+    setX(event.target.value);
+  }
+  function hundleOrganization(event: React.ChangeEvent<HTMLInputElement>) {
+    setOrganization(event.target.value);
+  }
+
+  //hundleTextColorついか
+  console.log(textColor);
+
   return (
     <>
       <main className={styles.main}>
-        <PrimaryBtn text="ログイン" />
-        <SecondaryBtn text="新規登録" />
-        <NameInput text="氏名"/>
-        <SNSInput text="@" onClick={()=>console.log('OK')}/>
-        <ColorPicker text="文字色"/>
+        <PrimaryBtn text="ログイン" onClick={() => console.log('PrimaryBtn Clicked')} />
+        <SecondaryBtn text="新規登録" onClick={() => console.log('Secondarybtn Clicked')} />
+        <NameInput
+          text="氏名"
+          value={name}
+          onChange={hundleName}
+        />
+        <NameInput
+          text="X"
+          value={x}
+          onChange={hundleX}
+        />
+        <NameInput
+          text="所属団体"
+          value={organization}
+          onChange={hundleOrganization}
+        />
+        <ColorPicker text="文字色" />
+        <ColorPicker text="背景色" />
       </main>
     </>
   )
