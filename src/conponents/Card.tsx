691c6df4f028ed193011648f330a33c87e8c7084
YukaChoco <sugar.ore.yk@gmail.com>
2023-09-18 04:14:59 +0900

    cardの色を再度修正

--- src/conponents/Card.tsx
+++ src/conponents/Card.tsx
@@ -10,33 +10,37 @@ import styles from '@/styles/Cards.module.css'
 
 
 const bull = (
-    <Box
-        component="span"
-        sx={{ display: 'inline-block', mx: '2px', transform: 'scale(0.8)' }}
-    >
-        •
-    </Box>
+  <Box
+    component="span"
+    sx={{ display: 'inline-block', mx: '2px', transform: 'scale(0.8)' }}
+  >
+    •
+  </Box>
 );
 
-export default function DisplayCard(props:Cards) {
-    return (
-        <Card className= {styles.cards}>
-            <CardContent>
-                <Typography className={styles.belong}>
-                    {props.organization}
-                </Typography>
+export default function DisplayCard(props: Cards) {
+  return (
+    <Card className={styles.cards} sx={{ backgroundColor: `${props.bgColor}` }}>
+      <CardContent>
+        <Typography className={styles.belong}>
+          {props.organization}
+        </Typography>
 
-                <Typography className={styles.username}>
-                    {props.name}
-                </Typography>
-                
-            </CardContent>
+        <Typography className={styles.username} sx={{ color: `${props.textColor}` }}>
+          {props.name}
+        </Typography>
 
-            {/* リンク遷移 */}
-            <CardActions disableSpacing className={styles.wrap}>
-                <Button size="small" className={styles.x}>{props.x}</Button>
-                <Button size="small" className={styles.instagram}>{props.instagram}</Button>
-            </CardActions>
-        </Card>
-    );
+      </CardContent>
+
+      {/* リンク遷移 */}
+      <CardActions disableSpacing className={styles.wrap}>
+        {props.x &&
+          <Button size="small" >{props.x}</Button>
+        }
+        {props.instagram &&
+          <Button size="small" >{props.instagram}</Button>
+        }
+      </CardActions>
+    </Card>
+  );
 }
