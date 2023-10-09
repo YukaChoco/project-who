1ae204f1f0f6ed7b87e35fb10d7798930ecb3d58
kotarowakuwaku <is0668fs@ed.ritsumei.ac.jp>
2023-10-10 00:02:34 +0900

    名刺作成画面仮完成

--- src/pages/make/createColor.tsx
+++ src/pages/make/createColor.tsx
@@ -7,12 +7,12 @@ import SecondaryBtn from '@/conponents/SecondaryBtn'
 import ColorPicker from '@/conponents/ColorPicker'
 import Header from '@/conponents/Header'
 import DisplayCard from '@/conponents/Card'
-import SwithButton from '@/conponents/SwitchButton'
+import SwitchButton from '@/conponents/SwitchButton'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Input() {
-        
+
 
   const [name, setName] = useState<string>('');
   const [x, setX] = useState<string>('');
@@ -41,7 +41,7 @@ export default function Input() {
     setBgColor(event.target.value);
   }
 
-  function hundleinstagram(event: React.ChangeEvent<HTMLInputElement > ){
+  function hundleinstagram(event: React.ChangeEvent<HTMLInputElement>) {
     setInstagram(event.target.value);
   }
 
@@ -49,39 +49,100 @@ export default function Input() {
   //   setOthers(event.target.value);
   // }
 
-  return (
-    <>
+  const [mode, setMode] = useState('入力');
 
-        <Header
-          onClick_edit={() => console.log('編集完了')}
-        />
-      
+    const handleAlignment = (
+        event: React.MouseEvent<HTMLElement>,
+        newAlignment: string | null,
+    ) => {
+        if (newAlignment !== null) {
+            setMode(newAlignment);
+        }
+    };
 
-      <main className={styles.main}>
-        
-        <div className={styles.preview}>
-          プレビュー
-        </div>
-    
+    let check = true;
 
-        <div className={styles.card}>
-       <DisplayCard  id={""} name={name} organization={organization} x={x} instagram={instagram} others={""} urlEnabled={false} textColor={textColor} bgColor={bgColor} onClickHandler={function (): void {
-          throw new Error('Function not implemented.')
-        } }/>
-        </div>
+    if(mode=="デザイン"){
+      let check = true;
+    }else{
+      let check = false;
+    }
 
-         <div className={styles.swith}> 
-        <SwithButton leftName={'入力'} rightName={'デザイン'} value={''}/>
-        </div>
+          return (
+            <>
 
-       
-        <ColorPicker text="文字色" value={textColor} onChange={hundleTextColor}/>
-        <ColorPicker text="背景色" value={bgColor} onChange={hundleBgColor}/>
+              <Header
+                onClick_edit={() => console.log('編集完了')}
+              />
 
-        <div className={styles.space}/>
-        <SecondaryBtn text="編集完了" onClick={() => console.log('Secondarybtn Clicked')} />
-        
-      </main>
-    </>
-  )
-}
+
+              <main className={styles.main}>
+
+                <div className={styles.preview}>
+                  <p>プレビュー</p>
+                </div>
+
+
+                <div className={styles.card}>
+                  <DisplayCard id={""} name={name} organization={organization} x={x} instagram={instagram} others={""} urlEnabled={false} textColor={textColor} bgColor={bgColor} onClickHandler={function (): void {
+                    throw new Error('Function not implemented.')
+                  }} />
+                </div>
+
+                <div className={styles.swith}>
+                  <SwitchButton leftName={'入力'} rightName={'デザイン'} value={''} onChange={handleAlignment}/>
+                </div>
+                  
+                <div className={styles.change}>
+                {
+                  (() => {
+                    if (mode=="デザイン") {
+                      return(
+                        <div>
+                          <ColorPicker text="文字色" value={textColor} onChange={hundleTextColor} />
+                          <ColorPicker text="背景色" value={bgColor} onChange={hundleBgColor} />
+
+                          <div className={styles.space} />
+                          <SecondaryBtn text="編集完了" onClick={() => console.log('Secondarybtn Clicked')} />
+                        </div>
+                      );
+                    } else {
+                      return (
+                        <div>
+                          <TextInput
+                            text="氏名"
+                            value={name}
+                            onChange={hundleName}
+                          />
+                          <TextInput
+                            text="instagram"
+                            value={instagram}
+                            onChange={hundleinstagram}
+                          />
+                          <TextInput
+                            text="X"
+                            value={x}
+                            onChange={hundleX}
+                          />
+                          {/* <TextInput
+                            text="その他"
+                            value={others}
+                            onChange={hundleother}
+                          /> */}
+                          <TextInput
+                            text="所属団体"
+                            value={organization}
+                            onChange={hundleOrganization}
+                          />
+                          <div className={styles.space}/>
+                          <SecondaryBtn text="編集完了" onClick={() => console.log('Secondarybtn Clicked')} />
+                        </div>
+                      );
+                    }
+                  })()
+                }
+              </div>
+              </main>
+            </>
+          )
+        }
