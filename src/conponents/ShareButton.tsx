77e8fc32385001a294b9075e6d3411e351d03ac7
YukaChoco <sugar.ore.yk@gmail.com>
2023-10-15 02:51:38 +0900

    ShareButton要らない部分削除

--- src/conponents/ShareButton.tsx
+++ src/conponents/ShareButton.tsx
@@ -1,35 +1,26 @@
 import * as React from 'react';
 import ShareOutlinedIcon from '@mui/icons-material/ShareOutlined';
-import styles from '@/styles/Login.module.css'
 import IconButton from '@mui/material/IconButton';
 import router from 'next/router';
 
 interface Props {
-    // onClick?: () => void;
-    // onClickHandler?: ()=>void;
-    // urlEnabled?: boolean;
-    id:string;
+    id: string;
 }
 
 export default function ShareButton(props: Props) {
-    // const onClick = props.urlEnabled ? props.onClickHandler : () => { };
-    // const URL = props.urlEnabled ? `${props.text}` : '';
     return (
         <IconButton
             onClick={() => router.push(`https://whooo.netlify.app/share/${props.id}`)}
             size='large'
             sx={{
                 position: 'fixed',
-                bottom: 40, /* 基準の位置を画面の一番下に指定する */
-                right: 40, /* 基準の位置を画面の一番右に指定する */
+                bottom: 40,
+                right: 40,
                 background: "#6F80BF",
                 color: "white",
                 ":hover": { background: "#6F80BF" },
             }}>
-            <ShareOutlinedIcon fontSize="large"/>
+            <ShareOutlinedIcon fontSize="large" />
         </IconButton>
-
-
-
     );
 }
