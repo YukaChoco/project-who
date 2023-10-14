398814afebf911b11b430c50266adb97fe2ba075
YukaChoco <sugar.ore.yk@gmail.com>
2023-10-15 02:07:39 +0900

    displaytextのcss修正、width:100%・その他

--- src/conponents/DisplayText.tsx
+++ src/conponents/DisplayText.tsx
@@ -6,12 +6,12 @@ import Link from 'next/link'
 
 export default function DisplayText(props: Details) {
   return (
-    <div className={styles.DisplayText_background}>
-      <div className={styles.DisplayText_title}>
+    <div className={styles.background}>
+      <div className={styles.title}>
         <p>{props.title}</p>
       </div>
-      <div className={styles.DisplayText_detail}>
-        <Link href={props.url || '/'}>{props.detail}</Link>
+      <div className={styles.detail}>
+        @<Link href={props.url || '/'}>{props.detail}</Link>
       </div>
     </div>
   )
