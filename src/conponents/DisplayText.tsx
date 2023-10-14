217012d616d78f7d1789802a4015c03871883bf1
YukaChoco <sugar.ore.yk@gmail.com>
2023-10-15 02:17:44 +0900

    containerを使用して縦並び時のborder指定

--- src/conponents/DisplayText.tsx
+++ src/conponents/DisplayText.tsx
@@ -6,7 +6,7 @@ import Link from 'next/link'
 
 export default function DisplayText(props: Details) {
   return (
-    <div className={styles.background}>
+    <div className={styles.container}>
       <div className={styles.title}>
         <p>{props.title}</p>
       </div>
