83207558b419a7f780a82aef4d2fa13e4644deda
YukaChoco <sugar.ore.yk@gmail.com>
2023-10-15 02:58:06 +0900

    isSNSIdで@の表示非表示切り替え

--- src/conponents/DisplayText.tsx
+++ src/conponents/DisplayText.tsx
@@ -2,16 +2,18 @@ import { Details } from '@/types/Details'
 import styles from '@/styles/DisplayText.module.css'
 import Link from 'next/link'
 
+interface Props extends Details {
+  isSNSId?: boolean;
+}
 
-
-export default function DisplayText(props: Details) {
+export default function DisplayText(props: Props) {
   return (
     <div className={styles.container}>
       <div className={styles.title}>
         <p>{props.title}</p>
       </div>
       <div className={styles.detail}>
-        @
+        {props.isSNSId && '@'}
         {
           props.url
             ?
