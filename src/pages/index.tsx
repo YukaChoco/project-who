21c057b2ce21a47499aa1dbce7368aeb3701f978
YukaChoco <sugar.ore.yk@gmail.com>
2023-10-16 17:48:08 +0900

    ログインページのみmargin削除

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,11 @@
 /* eslint-disable @next/next/no-page-custom-font */
 import Head from 'next/head'
-import { Inter } from 'next/font/google'
 import styles from '@/styles/Index.module.css'
-import Link from 'next/link'
 import FirebaseLogin from '@/utils/firebase/firebaseLogin'
 import { useRouter } from 'next/router';
-import PrimaryBtn from '@/conponents/PrimaryBtn'
-import { Button } from '@mui/material'
-
-const inter = Inter({ subsets: ['latin'] })
-
 
 export default function Index() {
   const router = useRouter();
-  const cardid = "card-test"
   return (
     <>
       <Head>
@@ -23,8 +15,6 @@ export default function Index() {
       </Head>
 
       <main className={styles.main}>
-
-
         <h1 className={styles.logo}>Who!</h1>
 
         <div className={styles.waku}>
@@ -44,9 +34,6 @@ export default function Index() {
 
         </div>
       </main>
-
-
-
     </>
   )
 }
