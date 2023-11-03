7a66c1e8061f69352afb24b7dc07b4f219491940
YukaChoco <sugar.ore.yk@gmail.com>
2023-11-03 22:28:55 +0900

    ShareButtonのpropsを削除・linkタグで囲う

--- src/conponents/ShareButton.tsx
+++ src/conponents/ShareButton.tsx
@@ -1,26 +1,23 @@
 import * as React from 'react';
 import ShareOutlinedIcon from '@mui/icons-material/ShareOutlined';
 import IconButton from '@mui/material/IconButton';
-import router from 'next/router';
+import Link from 'next/link';
 
-interface Props {
-    id: string;
-}
-
-export default function ShareButton(props: Props) {
+export default function ShareButton() {
     return (
-        <IconButton
-            onClick={() => router.push('/share')}
-            size='large'
-            sx={{
-                position: 'fixed',
-                bottom: 40,
-                right: 40,
-                background: "#6F80BF",
-                color: "white",
-                ":hover": { background: "#6F80BF" },
-            }}>
-            <ShareOutlinedIcon fontSize="large" />
-        </IconButton>
+        <Link href={'/share'} aria-label="名刺共有画面に移動">
+            <IconButton
+                size='large'
+                sx={{
+                    position: 'fixed',
+                    bottom: 40,
+                    right: 40,
+                    background: "#6F80BF",
+                    color: "white",
+                    ":hover": { background: "#6F80BF" },
+                }}>
+                <ShareOutlinedIcon fontSize="large" />
+            </IconButton>
+        </Link>
     );
 }
