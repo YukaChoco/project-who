01b5b012686bf296d240760066b0cf3b1787e1a9
YukaChoco <sugar.ore.yk@gmail.com>
2023-11-05 03:29:23 +0900

    getCardTypeの条件文をincludesに修正

--- src/utils/ok/getCardType.ts
+++ src/utils/ok/getCardType.ts
@@ -4,9 +4,15 @@ import getMyCardIds from './getMyCardIds';
 
 const getCardType = async (userId: string, cardId: string): Promise<CardType> => {
   const myCardIds = await getMyCardIds(userId);
-  if (myCardIds && myCardIds.findIndex((id) => id === cardId) !== -1) return CardType.My
+  if (myCardIds?.includes(cardId)) {
+    return CardType.My;
+  }
+
   const haveCardIds = await getHaveCardIds(userId);
-  if (haveCardIds && haveCardIds.findIndex((id) => id === cardId) !== -1) return CardType.Have
+  if (haveCardIds?.includes(cardId)) {
+    return CardType.Have;
+  }
+
   return CardType.None;
 }
 
