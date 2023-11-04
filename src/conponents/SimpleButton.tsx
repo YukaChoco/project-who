104086bbc1848b47a968b2104175a8642b89421c
mercy34mercy <etukobamasatyan@gmail.com>
2023-11-04 22:32:37 +0900

    sinpleのtypoを修正

--- src/conponents/SimpleButton.tsx
+++ src/conponents/SimpleButton.tsx
@@ -1,10 +1,7 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
 import styles from '@/styles/SimpleButton.module.css'
 import { Button } from '@mui/material'
 
-export default function SinpleButton() {
+export default function SimpleButton() {
   return (
     <Button className={styles.button}>Button</Button>
   )
