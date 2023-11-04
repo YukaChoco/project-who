c863784f2ec37a6d46101ba1f2a2397fcf31ffb9
YukaChoco <sugar.ore.yk@gmail.com>
2023-11-04 15:04:55 +0900

    getCardIdsで値がなければnullを返すよう修正

--- src/utils/ok/getHaveCardIds.tsx
+++ src/utils/ok/getHaveCardIds.tsx
@@ -2,13 +2,14 @@ import { db } from '@/firebase'
 import { doc, getDoc } from 'firebase/firestore'
 
 interface GetData {
-  haveCardIds: string[];
-  myCardIds: string[];
+  haveCardIds: string[] | undefined;
+  myCardIds: string[] | undefined;
 }
 
 export default async function getHaveCardIds(userId: string) {
   const querySnapshot = await getDoc(doc(db, 'users', userId))
   const fetchIds: GetData = querySnapshot.data() as GetData
-  const ids: string[] = fetchIds.haveCardIds;
+  if (fetchIds.haveCardIds === undefined) return null;
+  const ids = fetchIds.haveCardIds;
   return ids;
 }
--- src/utils/ok/getMyCardIds.tsx
+++ src/utils/ok/getMyCardIds.tsx
@@ -2,14 +2,14 @@ import { db } from '@/firebase'
 import { doc, getDoc } from 'firebase/firestore'
 
 interface GetData {
-  myMainCardId: string;
-  haveCardIds: string[];
-  myCardIds: string[];
+  haveCardIds: string[] | undefined;
+  myCardIds: string[] | undefined;
 }
 
 export default async function getMyCardIds(userid: string) {
   const querySnapshot = await getDoc(doc(db, 'users', userid))
   const fetchIds: GetData = querySnapshot.data() as GetData;
-  const ids: string[] = fetchIds.myCardIds;
+  if (fetchIds.myCardIds === undefined) return null;
+  const ids = fetchIds.myCardIds;
   return ids;
 }
