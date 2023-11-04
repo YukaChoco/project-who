32a4aff252496723641f80966a7d0283c3c4de70
mercy34mercy <etukobamasatyan@gmail.com>
2023-11-04 22:41:54 +0900

    simplebuttonごとなくす

--- src/conponents/SimpleButton.tsx
+++
@@ -1,8 +0,0 @@
-import styles from '@/styles/SimpleButton.module.css'
-import { Button } from '@mui/material'
-
-export default function SimpleButton() {
-  return (
-    <Button className={styles.button}>Button</Button>
-  )
-}
