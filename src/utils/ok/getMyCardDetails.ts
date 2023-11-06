8192bad8c8eaa133f33fae6086b99673a3ff252b
YukaChoco <sugar.ore.yk@gmail.com>
2023-11-07 01:08:01 +0900

    myCardDetailsのreturnをnullに変更

--- src/utils/ok/getMyCardDetails.ts
+++ src/utils/ok/getMyCardDetails.ts
@@ -5,7 +5,7 @@ import type { CardData } from '@/types/CardData'
 export default async function getMyCardDetails(userId: string) {
   const myCardIds = await getMyCardIds(userId);
 
-  if (!myCardIds) return [];
+  if (!myCardIds) return null;
 
   const fetchedDetails: (CardData | undefined)[] = await Promise.all(myCardIds.map(async (id) => {
     const fetchedDetail: CardData | null = await getCardDetils(id);
