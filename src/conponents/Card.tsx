c86fe1322c2e2907d6d7bc3a67c260add631c1b5
YukaChoco <sugar.ore.yk@gmail.com>
2023-11-08 11:47:10 +0900

    Cardのスタイルを修正

--- src/conponents/Card.tsx
+++ src/conponents/Card.tsx
@@ -3,10 +3,10 @@ import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
-import styles from '@/styles/Cards.module.css'
 import { getURL } from '@/utils/ok/getURL';
 import Link from 'next/link';
 import type { CardData } from '@/types/CardData';
+import { Box } from '@mui/material';
 
 interface Props extends CardData {
   urlEnabled: boolean;
@@ -15,24 +15,59 @@ interface Props extends CardData {
 
 export default function DisplayCard(props: Props) {
   const onClick = props.urlEnabled ? props.onClickHandler : () => { };
+
+  const cardStyle = {
+    backgroundColor: `${props.bgColor}`,
+    color: `${props.textColor}`,
+    fontSize: '1rem',
+    lineHeight: '1.1rem',
+    width: '100%',
+    padding: '20px',
+  }
+  const organizatinoStyle = {
+    minHeight: '2.4rem',
+  }
+  const nameStyle = {
+    textAlign: 'center',
+    margin: '20px 0px',
+    width: '100%',
+    fontSize: '1.5rem',
+    fontWeight: '600',
+  }
+  const SNSUrlContainerStyle = {
+    minHeight: '2.4rem',
+    padding: '0px',
+    display: 'grid',
+    gridTemplateRows: '1fr 1fr',
+    gridTemplateClumns: '1fr',
+  }
+  const SNSUrlStyle = {
+    minHeight: '1.2rem',
+  }
+
+
   return (
-    <Card className={styles.cards} sx={{ backgroundColor: `${props.bgColor}`, color: `${props.textColor}` }} onClick={onClick}>
+    <Card sx={cardStyle} onClick={onClick}>
       <CardContent>
-        <Typography className={styles.belong} >
+        <Typography sx={organizatinoStyle} >
           {props.organization}
         </Typography>
-        <Typography className={styles.username} >
+        <Typography sx={nameStyle} >
           {props.name}
         </Typography>
       </CardContent>
 
-      <CardActions disableSpacing className={styles.urlContainer}>
-        {props.x &&
-          <Link href={getURL('x', props.id)}>X@{props.x}</Link>
-        }
-        {props.instagram &&
-          <Link href={getURL('instagram', props.id)}>Instagram@{props.instagram}</Link>
-        }
+      <CardActions disableSpacing sx={SNSUrlContainerStyle}>
+        <Box sx={SNSUrlStyle}>
+          {props.x &&
+            <Link href={getURL('x', props.id)}>X@{props.x}</Link>
+          }
+        </Box>
+        <Box sx={SNSUrlStyle}>
+          {props.instagram &&
+            <Link href={getURL('instagram', props.id)}>Instagram@{props.instagram}</Link>
+          }
+        </Box>
       </CardActions>
     </Card >
   );
