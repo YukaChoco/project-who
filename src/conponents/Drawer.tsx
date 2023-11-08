cbd9c2225a568a0bca422a570b151b4f43a88738
YukaChoco <sugar.ore.yk@gmail.com>
2023-11-08 12:56:01 +0900

    Drawerのimport修正

--- src/conponents/Drawer.tsx
+++ src/conponents/Drawer.tsx
@@ -1,18 +1,9 @@
 import * as React from 'react';
-import Box from '@mui/material/Box';
-import SwipeableDrawer from '@mui/material/SwipeableDrawer';
-import Button from '@mui/material/Button';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemText from '@mui/material/ListItemText';
-import IconButton from '@mui/material/IconButton';
-import styles from '@/styles/Drawer.module.css'
+import { Box, SwipeableDrawer, List, ListItem, ListItemButton, ListItemText, IconButton } from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
 import Link from 'next/link'
 
 type Anchor = 'right';
-
 
 export default function Drawer() {
   const [state, setState] = React.useState({
@@ -41,21 +32,21 @@ export default function Drawer() {
       onKeyDown={toggleDrawer(anchor, false)}
     >
       <List className={styles.drawer}>
-          <ListItem key={'名刺の作成'} disablePadding>
-            <ListItemButton>
-              <Link href="/make/mycard"><ListItemText primary={'　名刺の作成　　　　　　'} className={styles.drawer_item} /></Link>
-            </ListItemButton>
-          </ListItem>
-          <ListItem key={'アカウントメモの追加'} disablePadding>
-            <ListItemButton>
+        <ListItem key={'名刺の作成'} disablePadding>
+          <ListItemButton>
+            <Link href="/make/mycard"><ListItemText primary={'　名刺の作成　　　　　　'} className={styles.drawer_item} /></Link>
+          </ListItemButton>
+        </ListItem>
+        <ListItem key={'アカウントメモの追加'} disablePadding>
+          <ListItemButton>
             <Link href="/make/card"><ListItemText primary={'　アカウントメモの追加　'} className={styles.drawer_item} /></Link>
-            </ListItemButton>
-          </ListItem>
-          <ListItem key={'￥Up grade'} disablePadding>
-            <ListItemButton>
+          </ListItemButton>
+        </ListItem>
+        <ListItem key={'￥Up grade'} disablePadding>
+          <ListItemButton>
             <Link href="/upgrade"><ListItemText primary={'　￥Up grade　　　　　　'} className={styles.drawer_item} /></Link>
-            </ListItemButton>
-          </ListItem>
+          </ListItemButton>
+        </ListItem>
       </List>
     </Box>
   );
@@ -65,16 +56,16 @@ export default function Drawer() {
       {(['right'] as const).map((anchor) => (
         <React.Fragment key={anchor} >
           <IconButton
-          onClick={toggleDrawer(anchor, true)}
-                size="large"
-                edge="start"
-                color="inherit"
-                aria-label="menu"
-                className={styles.Header_icon}
-                // onClick={() => console.log('menuBtn Clicked')}
-              >
-                <MenuIcon />
-              </IconButton>
+            onClick={toggleDrawer(anchor, true)}
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            className={styles.Header_icon}
+          // onClick={() => console.log('menuBtn Clicked')}
+          >
+            <MenuIcon />
+          </IconButton>
           <SwipeableDrawer
             anchor={anchor}
             open={state[anchor]}
