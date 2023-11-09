3578b34f9d84a5d8b20935beff9f5394a684a119
YukaChoco <sugar.ore.yk@gmail.com>
2023-11-09 14:53:01 +0900

    SNSInputのスタイル修正

--- src/conponents/SNSInput.tsx
+++ src/conponents/SNSInput.tsx
@@ -6,7 +6,6 @@ import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
 import Select, { SelectChangeEvent } from '@mui/material/Select';
 import { createSvgIcon } from '@mui/material/utils';
-import styles from '@/styles/SNSInput.module.css'
 
 interface SNSInputProps {
   labelText: string;
@@ -37,37 +36,70 @@ export default function SNSInput({
   const handleChange = (event: SelectChangeEvent) => {
     setSNS(event.target.value as string);
   };
+
+  //現状使用していないため、動作チェックしていません。スタイルを中心に確認してください。
+  const inputStyle = {
+    width: '100%',
+    margin: '0px 20px',
+  }
+  const containerStyle = {
+    textAlign: 'center',
+    backgroundColor: '#F4F5FC',
+    borderRadius: '5px',
+    width: '100%',
+    height: '125px',
+    margin: '5px 0',
+    minWidth: 120,
+  }
+  const snsContainerStyle = {
+    position: 'relative',
+  }
+  const selectStyle = {
+    float: 'left',
+    left: '0',
+    margin: '5px 10px 0 10px',
+    width: '42%',
+    backgroundColor: 'white',
+  }
+  const plusIconStyle = {
+    float: 'left',
+    position: 'absolute',
+    right: '0',
+    margin: '10px',
+    height: '40px',
+    width: '40px',
+  }
   return (
-    <>
-      <div className={styles.SNSInput_background}>
-        <Box sx={{ minWidth: 120 }} className={styles.SelectIconContainer}>
-          <FormControl fullWidth className={styles.SNSSelect}>
-            <InputLabel id="demo-simple-select-label">SNS</InputLabel>
-            <Select
-              labelId="demo-simple-select-label"
-              id="demo-simple-select"
-              value={SNS}
-              label="SNS"
-              onChange={handleChange}
-            >
-              <MenuItem value={10}>X</MenuItem>
-              <MenuItem value={20}>Instagram</MenuItem>
-              <MenuItem value={30}>Facebook</MenuItem>
-              <MenuItem value={40}>mail</MenuItem>
-            </Select>
-          </FormControl>
-          <PlusIcon className={styles.PlusIcon} onClick={onClick} />
-        </Box>
-        <TextField className={styles.SNSInput} label="@" variant="filled"
-          sx={{
-            input: {
-              color: "black",
-              background: "#F4F5FC"
-            }
-          }}
-          focused
-        />
-      </div>
-    </>
+    <Box sx={containerStyle}>
+      <Box sx={snsContainerStyle}>
+        <FormControl fullWidth sx={selectStyle}>
+          <InputLabel id="demo-simple-select-label">SNS</InputLabel>
+          <Select
+            labelId="demo-simple-select-label"
+            id="demo-simple-select"
+            value={SNS}
+            label="SNS"
+            onChange={handleChange}
+          >
+            <MenuItem value={10}>X</MenuItem>
+            <MenuItem value={20}>Instagram</MenuItem>
+            <MenuItem value={30}>Facebook</MenuItem>
+            <MenuItem value={40}>mail</MenuItem>
+          </Select>
+        </FormControl>
+        <PlusIcon sx={plusIconStyle} onClick={onClick} />
+      </Box>
+      <TextField label="@" variant="filled"
+        sx={{
+          ...inputStyle,
+          input: {
+            color: "black",
+            background: "#F4F5FC"
+          }
+        }
+        }
+        focused
+      />
+    </Box>
   )
 }
