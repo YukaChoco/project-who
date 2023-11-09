5b6d76c6e3d93f9e0b8bd13a2b2ff8fd9871085c
YukaChoco <sugar.ore.yk@gmail.com>
2023-11-09 11:10:57 +0900

    PrimaryButtonのスタイル修正

--- src/conponents/PrimaryButton.tsx
+++ src/conponents/PrimaryButton.tsx
@@ -1,15 +1,26 @@
-import styles from '@/styles/PrimaryButton.module.css'
 import { Button } from '@mui/material'
 
 interface Props {
-    text: string;
-    onClick?: () => void;
+  text: string;
+  onClick?: () => void;
 }
 
 export default function PrimaryButton(props: Props) {
+  const buttonStyle = {
+    display: 'inline-block',
+    width: '100%',
+    backgroundColor: '#fff',
+    fontSize: '1.125rem',
+    border: '1.5px solid #969696',
+    borderRadius: '45px',
+    color: '#6F80BF',
+    textDecoration: 'none',
+    ':hover': {
+      border: '1.5px solid',
+      backgroundColor: '#DCDBE8',
+    },
+  }
   return (
-    <>
-        <Button variant="outlined" className={styles.PrimaryButton} onClick={props.onClick}>{props.text}</Button>
-    </>
+    <Button variant="outlined" sx={buttonStyle} onClick={props.onClick}>{props.text}</Button>
   )
 }
