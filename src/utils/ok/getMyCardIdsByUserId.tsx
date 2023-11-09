c07a1eb184044a31a3f66fa1a4d696d02070a46c
YukaChoco <sugar.ore.yk@gmail.com>
2023-11-09 16:05:31 +0900

    GetUserFieldsをUserFieldsに修正

--- src/utils/ok/getMyCardIdsByUserId.tsx
+++ src/utils/ok/getMyCardIdsByUserId.tsx
@@ -1,14 +1,14 @@
 import { db } from '@/firebase'
 import { doc, getDoc } from 'firebase/firestore'
 
-interface GetUserFields {
+interface UserFields {
   haveCardIds: string[] | undefined;
   myCardIds: string[] | undefined;
 }
 
 export default async function getMyCardIdsByUserId(userId: string) {
   const userFieldsQuery = await getDoc(doc(db, 'users', userId));
-  const fetchUserFields: GetUserFields = userFieldsQuery.data() as GetUserFields;
+  const fetchUserFields: UserFields = userFieldsQuery.data() as UserFields;
   if (fetchUserFields.myCardIds === undefined) return null;
   const myCardIds = fetchUserFields.myCardIds;
   return myCardIds;
