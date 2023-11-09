74fbec3f875cdc40cacfa109c7a478e87f4ded46
YukaChoco <sugar.ore.yk@gmail.com>
2023-11-09 09:46:49 +0900

    NewCardの名称修正・Linkに変更

--- src/conponents/NewCard.tsx
+++ src/conponents/NewCard.tsx
@@ -1,21 +1,20 @@
 import * as React from 'react';
 import DisplayCard from './Card';
-import router from 'next/router'
+import Link from 'next/link';
 
-export default function MakeNewCard() {
+export default function NewCard() {
     return (
-        <DisplayCard
-            id=""
-            name="＋新規作成"
-            organization=""
-            x=""
-            instagram=""
-            others=""
-            urlEnabled={true}
-            textColor="#969696"
-            bgColor="#F7F7F7"
-            onClickHandler={() => router.push("/make/mycard")}
-        />
+        <Link href='/make/mycard'>
+            <DisplayCard
+                name="＋新規作成"
+                organization=""
+                x=""
+                instagram=""
+                urlEnabled
+                textColor="#969696"
+                bgColor="#F7F7F7"
+            />
+        </Link>
     )
 }
 
