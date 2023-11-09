c43343146d09ff4462eb45efbb2411f527742feb
YukaChoco <sugar.ore.yk@gmail.com>
2023-11-09 11:19:02 +0900

    SecondaryButtonのスタイル修正

--- src/conponents/SecondaryButton.tsx
+++ src/conponents/SecondaryButton.tsx
@@ -1,15 +1,28 @@
-import styles from '@/styles/SecondaryButton.module.css'
 import { Button } from '@mui/material'
 
 interface Props {
-    text: string;
-    onClick?: () => void;
+  text: string;
+  onClick?: () => void;
 }
 
 export default function SecondaryButton(props: Props) {
+  const buttonStyle = {
+    display: 'inline-block',
+    width: '100%',
+    fontSize: '1.125rem',
+    border: '1.5px solid #fff',
+    borderRadius: '45px',
+    color: '#fff',
+    backgroundColor: ' #6F80BF',
+    textDecoration: 'none',
+    ':hover': {
+      border: '1.5px solid #fff',
+      backgroundColor: 'rgba(255,255,255,0.2)',
+    },
+  }
   return (
     <>
-        <Button variant="outlined" className={styles.SecondaryButton} onClick={props.onClick}>{props.text}</Button>
+      <Button variant="outlined" sx={buttonStyle} onClick={props.onClick}>{props.text}</Button>
     </>
   )
 }
