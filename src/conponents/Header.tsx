645079b525657a562192006faf64973ba071f028
kotarowakuwaku <is0668fs@ed.ritsumei.ac.jp>
2023-11-11 22:32:07 +0900

    Header module をsxに変えてみた

--- src/conponents/Header.tsx
+++ src/conponents/Header.tsx
@@ -7,7 +7,7 @@ import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import SearchIcon from '@mui/icons-material/Search';
-import styles from '@/styles/Header.module.css'
+// import styles from '@/styles/Header.module.css'
 import Head from 'next/head';
 import Drawer from '@/conponents/Drawer'
 import Link from 'next/link'
@@ -21,48 +21,78 @@ interface Props {
 
 export default function Header(props: Props) {
   return (
-    <div>
-      <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link href="https://fonts.googleapis.com/css2?family=Kiwi+Maru:wght@300&family=Lemon&display=swap" rel="stylesheet" />
-      </Head>
+    <>
+      <div>
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link href="https://fonts.googleapis.com/css2?family=Kiwi+Maru:wght@300&family=Lemon&display=swap" rel="stylesheet" />
+        </Head>
 
-      <Box sx={{ flexGrow: 1 }} className={styles.Header_container}>
-        <AppBar position="static" className={styles.Header_bar} sx={{ height: '58px' }}>
-          <Toolbar>
-            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-              <Link href="/cards" className={styles.font}>Who!</Link>
-            </Typography>
-            {
-              (props.useSearchIcon) &&
-              <IconButton
-                size="large"
-                color="inherit"
-                className={styles.Header_icon}
-              >
-                <Link href="/upgrade"><SearchIcon /></Link>
-              </IconButton>
-            }
+        <Box
+          sx={{
+            flexGrow: 1,
+            width: '100%',  // Use string value for percentage
+            position: 'fixed',
+            top: 0,
+            left: 0,
+            zIndex: 1
+          }}
+        >
 
-            {
-              (props.useMenuIcon) &&
-              <><Drawer /></>
-            }
+          <AppBar
+            position="static"
+            sx={{
+              height: '58px',
+              backgroundColor: '#6F80BF'  // Use camelCase here
+            }}
+          >
 
-            {
-              (props.onClick_edit != null) &&
-              <Button color="inherit" onClick={props.onClick_edit}>編集完了</Button>
-            }
+            <Toolbar>
+              <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                <Link
+                  href="/cards"
+                  style={{
+                    fontFamily: "'Lemon', serif, cursive",
+                  }}
+                >
+                  Who!
+                </Link>
 
-            {
-              (props.onClick_register != null) &&
-              <Button color="inherit" onClick={props.onClick_register}>登録</Button>
-            }
-          </Toolbar>
-        </AppBar>
-      </Box>
-      <div className={styles.Header_margin}></div>
-    </div>
+
+              </Typography>
+              {
+                (props.useSearchIcon) &&
+                <IconButton
+                  size="large"
+                  color="inherit"
+                  style={{
+                    margin: '0 3px 0 0',
+                  }}
+                >
+                  <Link href="/upgrade"><SearchIcon /></Link>
+                </IconButton>
+              }
+
+              {
+                (props.useMenuIcon) &&
+                <><Drawer /></>
+              }
+
+              {
+                (props.onClick_edit != null) &&
+                <Button color="inherit" onClick={props.onClick_edit}>編集完了</Button>
+              }
+
+              {
+                (props.onClick_register != null) &&
+                <Button color="inherit" onClick={props.onClick_register}>登録</Button>
+              }
+            </Toolbar>
+          </AppBar>
+        </Box>
+        <div ></div>
+      </div>
+    </>
   );
 }
