ffee61ecbe7d6659da0fe65d2c571e32a3277709
YukaChoco <sugar.ore.yk@gmail.com>
2023-11-27 19:05:41 +0900

    ColorPickerにrequired,error,helperTextを追加

--- src/components/ColorPicker.tsx
+++ src/components/ColorPicker.tsx
@@ -1,13 +1,23 @@
-import { Box } from '@mui/material';
+import { Box, FormHelperText, FormLabel } from '@mui/material';
 import * as React from 'react';
 
 interface ColorPickerProps {
   labelText: string;
   value: string;
   onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  required?: boolean;
+  error?: boolean;
+  helperText?: string;
 }
 
-export default function ColorPicker({ labelText = '色の指定', value = '#FFF', onChange = () => {} }: ColorPickerProps) {
+export default function ColorPicker({
+  labelText = '色の指定',
+  value = '#FFF',
+  onChange = () => {},
+  required = false,
+  error = false,
+  helperText = '',
+}: ColorPickerProps) {
   const containerStyle = {
     backgroundColor: '#F4F5FC',
     borderRadius: '5px',
@@ -27,11 +37,18 @@ export default function ColorPicker({ labelText = '色の指定', value = '#FFF'
   };
 
   return (
-    <Box sx={containerStyle}>
-      <label htmlFor={labelText} style={labelStyle}>
-        {labelText}
-      </label>
-      <input type='color' id={labelText} value={value} onChange={onChange} style={inputStyle} />
+    <Box>
+      <Box sx={containerStyle}>
+        <FormLabel htmlFor={labelText} style={labelStyle} error={error} required={required}>
+          {labelText}
+        </FormLabel>
+        <input type='color' id={labelText} value={value} onChange={onChange} style={inputStyle} />
+      </Box>
+      {helperText && (
+        <FormHelperText sx={{ padding: '0px 10px' }} error={error} required={required}>
+          {helperText}
+        </FormHelperText>
+      )}
     </Box>
   );
 }
--- src/components/EditColors.tsx
+++ src/components/EditColors.tsx
@@ -8,10 +8,17 @@ interface EditColorsProps {
 }
 
 export default function EditColors(props: EditColorsProps) {
+  const colorsValidate = (textColor: string, bgColor: string) => {
+    if (textColor === bgColor) {
+      return { error: true, helperText: '文字色と背景色は別の色を指定してください' };
+    }
+    return { error: false, helperText: '' };
+  };
+  const { error, helperText } = colorsValidate(props.textColor, props.bgColor);
   return (
     <>
-      <ColorPicker labelText='文字色' value={props.textColor} onChange={props.handleTextColor} />
-      <ColorPicker labelText='背景色' value={props.bgColor} onChange={props.handleBgColor} />
+      <ColorPicker labelText='文字色' value={props.textColor} onChange={props.handleTextColor} error={error} />
+      <ColorPicker labelText='背景色' value={props.bgColor} onChange={props.handleBgColor} error={error} helperText={helperText} />
     </>
   );
 }
