0705523444c454ebea6bcdf2e010bb8c976a99d2
YukaChoco <sugar.ore.yk@gmail.com>
2023-12-09 23:05:48 +0900

    addCardIdのコードを修正

--- src/utils/ok/addCardId.ts
+++ src/utils/ok/addCardId.ts
@@ -3,15 +3,10 @@ import { db } from '@/firebase';
 import { CARD_TYPE, CardType } from '@/types/CardType';
 
 export default async function addCardId(userId: string, cardId: string, cardType: CardType) {
+  const field = cardType === CARD_TYPE.My ? 'myCardIds' : 'haveCardIds';
   const docRef = doc(db, 'users', userId);
-  if (cardType === CARD_TYPE.My) {
-    updateDoc(docRef, {
-      myCardIds: arrayUnion(cardId),
-    });
-  } else if (cardType === CARD_TYPE.Have) {
-    updateDoc(docRef, {
-      haveCardIds: arrayUnion(cardId),
-    });
-  }
+  await updateDoc(docRef, {
+    [field]: arrayUnion(cardId),
+  });
   return true;
 }
