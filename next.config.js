b2b137242264f9a5acc6a9744ce71967a6fe2dc0
YukaChoco <sugar.ore.yk@gmail.com>
2023-12-18 18:25:36 +0900

    requireでのインポートをwithPWAに修正

--- next.config.js
+++ next.config.js
@@ -1,13 +1,14 @@
-/** @type {import('next').NextConfig} */
+import withPWA from 'next-pwa';
 
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const withPWA = require('next-pwa')({
+const nextConfig = {
+  reactStrictMode: true,
+};
+
+const pwaConfig = withPWA({
   dest: 'public',
   register: true,
   skipWaiting: true,
   disable: process.env.NODE_ENV === 'development',
 });
 
-module.exports = withPWA({
-  reactStrictMode: true,
-});
+export default pwaConfig(nextConfig);
