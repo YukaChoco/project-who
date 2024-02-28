330a90ced50ef50b5b987ce16a4b7d934d83856d
YukaChoco <sugar.ore.yk@gmail.com>
2024-02-29 07:43:00 +0900

    テキストのコピーを追加

--- src/pages/mycards.tsx
+++ src/pages/mycards.tsx
@@ -1,4 +1,4 @@
-import { Box, Modal } from '@mui/material';
+import { Box, Button, Modal } from '@mui/material';
 import Head from 'next/head';
 import router from 'next/router';
 import { useEffect, useState } from 'react';
@@ -119,6 +119,14 @@ export default function Detail() {
   };
   const shareURL = `${window.location.origin}/card/${cardData.id}`;
 
+  const copy = async () => {
+    try {
+      await navigator.clipboard.writeText(shareURL);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <>
       <Head>
@@ -163,6 +171,11 @@ export default function Detail() {
                   <EmailShareButton url={shareURL}>
                     <EmailIcon size={64} round />
                   </EmailShareButton>
+                </Box>
+                <Box sx={snsContainer}>
+                  <Button onClick={copy} type='button' sx={{ borderBottom: '1px' }}>
+                    リンクをコピーする
+                  </Button>
                 </Box>
               </div>
             </div>
