14564870cefe7efb049634b7bac578c52f319fb2
YukaChoco <sugar.ore.yk@gmail.com>
2024-02-29 11:54:41 +0900

    ogp設定

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,6 +8,15 @@ class MyDocument extends Document {
           <link rel='manifest' href='/manifest.json' />
           <link rel='apple-touch-icon' href='/icon.png' />
           <meta name='theme-color' content='#fff' />
+          <meta property='og:url' content='https://project-who.vercel.app' />
+          <meta property='og:title' content='Who!大学生のためのSNS管理ツール' />
+          <meta
+            property='og:description'
+            content='大学生に向けたオンライン名刺交換アプリケーション「Who!」です。SNSのアカウントなどのプロフィール情報を、名刺で交換することができます。'
+          />
+          <meta property='og:site_name' content='Who!' />
+          <meta property='og:image' content='/ogp.png' />
+          <meta name='twitter:card' content='/ogp.png' />
         </Head>
         <body>
           <Main />
