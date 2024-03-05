53fb766b73e1b38714db36e0800f68dc012f14f5
kotarowakuwaku <is0668fs@ed.ritsumei.ac.jp>
2024-03-06 00:47:39 +0900

    検索バーコンポーネントのデザイン仮完成

---
+++ src/components/SearchBar.tsx
@@ -0,0 +1,59 @@
+import SearchIcon from '@mui/icons-material/Search';
+import InputBase from '@mui/material/InputBase';
+import { styled, alpha } from '@mui/material/styles';
+import * as React from 'react';
+
+const Search = styled('div')(({ theme }) => ({
+  position: 'relative',
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  '&:hover': {
+    backgroundColor: alpha(theme.palette.common.white, 0.25),
+  },
+  marginLeft: 0,
+  width: '100%',
+}));
+
+const SearchIconWrapper = styled('div')(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  width: '100%',
+  height: '100%',
+  position: 'absolute',
+  pointerEvents: 'none',
+  display: 'flex',
+  alignItems: 'center',
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: 'inherit',
+  width: '100%',
+  border: '1px solid #969696',
+  borderRadius: '45px',
+  '& .MuiInputBase-input': {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create('width'),
+    [theme.breakpoints.up('sm')]: {
+      width: '80%',
+      '&:focus': {
+        width: '90%',
+      },
+    },
+  },
+}));
+
+export default function SearchBar() {
+  return (
+    <Search>
+      <SearchIconWrapper>
+        <SearchIcon
+          sx={{
+            color: '#969696',
+          }}
+        />
+      </SearchIconWrapper>
+      <StyledInputBase placeholder='検索' inputProps={{ 'aria-label': 'search' }} />
+    </Search>
+  );
+}
