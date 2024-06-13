c556298d7d291404f4afbcf4ee18b7ddabe8a61b
YukaChoco <sugar.ore.yk@gmail.com>
2024-06-13 18:54:07 +0900

    登録処理をawait

--- src/utils/ok/addHaveCardId.ts
+++ src/utils/ok/addHaveCardId.ts
@@ -3,8 +3,16 @@ import { db } from '@/firebase';
 
 export default async function addHaveCardId(userId: string, haveCardId: string) {
   const docRef = doc(db, 'users', userId);
-  updateDoc(docRef, {
+  await updateDoc(docRef, {
     haveCardIds: arrayUnion(haveCardId),
-  });
+  })
+    .then(() => {
+      console.log('Document successfully updated!');
+    })
+    .catch((error) => {
+      console.error('Error updating document: ', error);
+      return false;
+    });
+
   return true;
 }
